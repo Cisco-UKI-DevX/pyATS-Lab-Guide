1e580fed1790150215266cea1e941f2833bb2ae1
Stuart Traynor <sttrayno@cisco.com>
2020-11-10 13:45:29 +0000

    Update custom_route_test_job.py

--- tests/exercise4/custom_route_test/custom_route_test_job.py
+++ tests/exercise4/custom_route_test/custom_route_test_job.py
@@ -1,12 +1,6 @@
 # see https://pubhub.devnetcloud.com/media/pyats/docs/easypy/jobfile.html
 # for how job files work
 
-__author__ = "Oren Brigg"
-__copyright__ = "Copyright (c) 2020, Cisco Systems Inc."
-__contact__ = ["obrigg@cisco.com"]
-__credits__ = ["hapresto@cisco.com"]
-__version__ = 1.0
-
 import os
 from pyats.easypy import run
 
