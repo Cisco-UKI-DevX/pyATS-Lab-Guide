b202fdc8592b06dabdf5abb3ff79b283b4571ff9
Stuart Traynor <sttrayno@cisco.com>
2020-11-11 15:10:29 +0000

    Update example.py

--- tests/exercise3/example.py
+++ tests/exercise3/example.py
@@ -3,7 +3,7 @@ from genie.utils import Dq
 from genie.testbed import load
 import json
 
-tb = load('./testbed/testbed-devices.yaml')       # Load our testbed file from a file.
+tb = load('../../testbed/testbed-devices.yaml')       # Load our testbed file from a file.
 dev = tb.devices['internet-rtr01']       # Define an object called dev from the device named csr1000v
 
 dev.connect()      # Connect to the object dev we defined earlier -  this must be done before parsing to the device
