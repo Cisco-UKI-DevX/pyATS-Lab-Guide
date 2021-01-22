ed51a58c5ddb44a1bc3650841c8595e3671a179a
Stuart Traynor <sttrayno@cisco.com>
2021-01-22 11:31:08 +0000

    Update example.py

--- tests/exercise3/example.py
+++ tests/exercise3/example.py
@@ -3,7 +3,7 @@ from genie.utils import Dq
 from genie.testbed import load
 import json
 
-tb = load('../../testbed/testbed-devices.yaml')       # Load our testbed file from a file.
+tb = load('../../testbed/sandbox-devices.yaml')       # Load our testbed file from a file.
 dev = tb.devices['internet-rtr01']       # Define an object called dev from the device named csr1000v
 
 dev.connect()      # Connect to the object dev we defined earlier -  this must be done before parsing to the device
@@ -12,9 +12,9 @@ routingTable = dev.parse('show ip route')        # Run the command "show ip rout
 
 routes = len(routingTable['vrf']['default']['address_family']['ipv4']['routes']) # Work out how many routes are in the routing table by working out the length of the routes list from the datastructure returned by pyATS
 
-if routes == 3:
+if routes == 5:
    print("Pass: The expected number of routes are in the oruting table")
-elif routes < 3:
+elif routes < 5:
    print("Fail: There are less routes in the routing table than expected")
-elif routes > 3:
+elif routes > 5:
    print("Fail: There are more routes in the routing table than expected")
